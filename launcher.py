ca53362e6c27fa4bae85aef89c14e2c808287b0c
Vinamr Grover <vinamrgrover@gmail.com>
2022-09-10 03:04:46 +0530

    removed and added files

--- launcher.py
+++ launcher.py
@@ -1,16 +1,12 @@
-from dotenv import load_dotenv
-import os
-import bot.bot as _BotClass
+import bot.bot as BotClass
 
 if __name__ == '__main__':
-    load_dotenv(dotenv_path = 'token.env')
-
-    TOKEN = os.getenv('DISCORD_TOKEN')
-
-    Bot = _BotClass.Bot()
-
     try:
-        Bot.client.run(TOKEN)
+        Bot = BotClass.Bot()
+
+        with open('token.txt', 'r') as TOKEN:
+            Bot.client.run(TOKEN.read())
+
     except Exception as e:
         print(e)
 

0b10fd1805bc93b84a55eb1451ec7118f6239dd9
Vinamr Grover <vinamrgrover@gmail.com>
2022-09-17 01:32:41 +0530

    added help command

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,9 @@
 from nextcord.ext import commands
 import nextcord
-import random
-import os
+import random, os, json
 from dotenv import load_dotenv
+from nextcord import Embed, ButtonStyle
+from nextcord.ui import Button, View
 
 try:
 
@@ -29,6 +30,59 @@ async def toss(ctx):
 async def ping(ctx):
     await ctx.send(f'{round(client.latency * 1000, 2)} ms!')
 
+try:
+    client.remove_command('help')  # to remove the default help command
+    help_commands = json.load(open('../help.json'))
+except Exception as e:
+    print(e)
+
+
+def createHelpPages(page : int):
+    page = page % len(help_commands)  # to avoid out of range
+    title = list(help_commands)[page]
+    embed = Embed(color = 0xFF512C, title = title)
+    for key, val in help_commands[title].items():
+        embed.add_field(name = client.command_prefix + key, value = val, inline = False)
+        embed.set_footer(text = f'Page {page + 1} of {len(help_commands)}')
+    return embed
+
+
+@client.command(name = 'help')
+async def help(ctx):
+    current_page = 0
+
+    async def next_callback(interaction):
+        nonlocal current_page, sent_message
+        current_page += 1
+        await sent_message.edit(embed = createHelpPages(current_page), view = help_view)
+
+    async def prev_callback(interaction):
+        nonlocal current_page, sent_message
+        current_page -= 1
+        await sent_message.edit(embed = createHelpPages(current_page), view = help_view)
+
+    next_button = Button(label = '>', style = ButtonStyle.danger)
+    next_button.callback = next_callback
+    prev_button = Button(label = '<', style = ButtonStyle.danger)
+    prev_button.callback = prev_callback
+
+    help_view = View(timeout=100)
+    help_view.add_item(prev_button)
+    help_view.add_item(next_button)
+
+    sent_message = await ctx.send(embed = createHelpPages(current_page), view = help_view)
+
+
+# for testing button
+@client.command(name = 'temp')
+async def temp(ctx):
+    embed = Embed(color = 0xFF512C, title = 'Sample')
+    button = Button(label = '>', style = ButtonStyle.blurple)
+    view = View(timeout = 100)
+    view.add_item(button)
+
+    message_sent = await ctx.send(embed = embed, view = view)
+
 
 @client.event
 async def on_ready():
@@ -36,11 +90,10 @@ async def on_ready():
 
 
 try:
-    load_dotenv(dotenv_path = '../path.env')
+    load_dotenv(dotenv_path = 'path.env')
     PATH = os.getenv('TOKEN_PATH')
     with open(PATH, 'r') as TOKEN:
         client.run(TOKEN.read())
         TOKEN.close()
 except Exception as e:
     print(e)
-
